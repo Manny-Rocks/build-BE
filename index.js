e0ffd19594de0917728c38cbdad5cdd60037223e
Emmanuel Tanifum <etantanifum@gmail.com>
2019-11-20 03:46:15 -0500

    new port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const server = require("./api/server");
 
-const port = process.env.PORT || 4882;
+const port = process.env.PORT || 4832;
 server.listen(port, () => {
   console.log(`\n** Running on port ${port} **\n`);
 });
